4f4ba64ed8cea4c3e8b1e2038edcc45a0efa5b21
Ritika Singh <ritika.singh2019@vitstudent.ac.in>
2023-01-03 17:12:21 +0530

    ques1

---
+++ Microsoft/PolishNotation.cpp
@@ -0,0 +1,3 @@
+//You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+//Evaluate the expression. Return an integer that represents the value of the expression.
+
