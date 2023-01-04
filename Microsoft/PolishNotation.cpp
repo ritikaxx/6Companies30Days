2554515260304b8e32729dac245809ed3cf16b6b
Ritika Singh <ritika.singh2019@vitstudent.ac.in>
2023-01-04 14:04:49 +0530

    ques1

--- Microsoft/PolishNotation.cpp
+++ Microsoft/PolishNotation.cpp
@@ -1,3 +1,32 @@
 //You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
 //Evaluate the expression. Return an integer that represents the value of the expression.
 
+class Solution {
+
+    long resolves(int a, int b, char Op){
+        if(Op == '+') return a + b;
+        else if(Op == '-') return a - b;
+        else if(Op == '*') return (long)a*b;
+        return a/b;
+    }
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<long> S;
+        int n = tokens.size();
+        for(int i = 0; i < n; i++){
+            // check if element is an op
+            if(tokens[i].size() == 1 and tokens[i][0] < 48){
+                long n2 = S.top();
+                S.pop();
+                long n1 = S.top();
+                S.pop();
+                
+                string Op = tokens[i];
+                long val = resolves(n1, n2 , Op[0]);
+                S.push(val);
+            }else 
+                S.push(stol(tokens[i]));
+        }
+        return S.top();
+    }
+};
